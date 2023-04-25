0b40de105eeeb218a028b7f5aba7324acce47fe3
Hevrica <xoqja7951@naver.com>
2023-04-25 16:15:06 +0900

    최초 연결

---
+++ itinerary_be/src/main/java/itinerary/itinerary_be/ItineraryBeApplication.java
@@ -0,0 +1,13 @@
+package itinerary.itinerary_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItineraryBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItineraryBeApplication.class, args);
+	}
+
+}
---
+++ itinerary_be/src/test/java/itinerary/itinerary_be/ItineraryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package itinerary.itinerary_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItineraryBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
